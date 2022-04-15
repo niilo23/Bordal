1e2b4080a7fc93fe76fee74dbcaaee1616b4e8c5
Niilo <56909449+niilo23@users.noreply.github.com>
2022-04-16 00:22:06 +0300

    Initial Commit
    Game to GitHub

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSens = 100f;
+
+    public Transform playerBody;
+
+    float xRot = 0;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY  = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRot += mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
